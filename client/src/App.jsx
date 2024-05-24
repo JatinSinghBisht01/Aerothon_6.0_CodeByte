3aaa76cbed46d669ccefdb219c29855125ee0544
Abhinav Malhotra <abhinavmalhotra65521@gmail.com>
2024-05-24 18:26:51 +0530

    3 pages done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,24 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import LandingPage from "./pages/LandingPage";
+import ContactPage from "./pages/ContactPage";
+import AboutPage from "./pages/AboutPage";
 
 function App() {
   return (
     <Router>
       <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-        <Navbar/>
+        <Navbar />
       </div>
-      {/* <Routes>
-        <Route path="/" element={LandingPage}/>
-        <Route path="/about" element={AboutPage}/>
-        <Route path="/contact" element={ContactPage}/>
-        <Route path="/check-fault" element={CheckFaultPage}/>
-        <Route path="/chatbot" element={ChatbotPage}/>
-      </Routes> */}
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/contact" element={<ContactPage/>}/>
+         <Route path="/about" element={<AboutPage/>}/>
+        {/*<Route path="/check-fault" element={CheckFaultPage}/>
+        <Route path="/chatbot" element={ChatbotPage}/> */}
+      </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,41 +3,32 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const navList = (
-    <ul className="flex flex-col gap-2 lg:flex-row lg:gap-10">
+    <ul className="flex flex-col gap-2 lg:flex-row lg:gap-10 ">
       <li className={` text-[18px] font-medium cursor-pointer`}>
         <Button>
-          <Link to="/about">About Us</Link>
+          <Link to="/about" className="text-[#4dcbe4]">About Us</Link>
         </Button>
       </li>
       <li className={` text-[18px] font-medium cursor-pointer`}>
         <Button>
-          <Link to="/contact">Contact</Link>
+          <Link to="/contact" className="text-[#4dcbe4]">Contact</Link>
         </Button>
       </li>
       <li className={` text-[18px] font-medium cursor-pointer`}>
         <Button>
-          <Link to="/chatbot">Chatbot</Link>
+          <Link to="/chatbot" className="text-[#4dcbe4]">Chatbot</Link>
         </Button>
       </li>
       <li className={` text-[18px] font-medium cursor-pointer`}>
         <Button>
-          <Link to="/check-fault">Check Fault</Link>
+          <Link to="/check-fault" className="text-[#4dcbe4]">Check Fault</Link>
         </Button>
       </li>
       <li className={` text-[18px] font-medium cursor-pointer`}>
         <Button>
-          <Link to="/">Home</Link>
+          <Link to="/" className="text-[#4dcbe4]">Home</Link>
         </Button>
       </li>
-      {/* <li className={` text-[18px] font-medium cursor-pointer`}>
-            {isLoggedIn ? (
-              <Button onClick={handleLogout}>Logout</Button>
-            ) : (
-              <Button>
-                <Link to="/login">Login</Link>
-              </Button>
-            )}
-          </li> */}
     </ul>
   );
 
@@ -46,7 +37,7 @@ const Navbar = () => {
       className={`sm:px-16 px-6 w-full flex flex-col py-5 fixed top-0 z-20 backdrop-filter backdrop-blur-lg`}
     >
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
-        <div className={` text-[18px] font-medium cursor-pointer`}>
+        <div className={`text-white text-[18px] font-medium cursor-pointer`}>
           CodeByte x Aerothon
         </div>
         <div>{navList}</div>
