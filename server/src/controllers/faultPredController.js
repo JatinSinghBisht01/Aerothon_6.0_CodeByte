2695ed629f094f0a4ea48bd7a895ea8095393f72
Jatin Singh Bisht <96527048+JatinSinghBisht01@users.noreply.github.com>
2024-05-25 09:58:01 +0530

    minor

--- server/src/controllers/faultPredController.js
+++ server/src/controllers/faultPredController.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const predict = (req, res) => {
     const image = req.file;
-    console.log(image);
+    // console.log(image);
     res.status(200).json({message: 'Image uploaded successfully'});
 }
 
